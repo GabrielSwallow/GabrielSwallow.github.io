2654d28b2292803ec27282138097fdb09a5c8376
GabrielSwallow <gabrielswallow@googlemail.com>
2022-09-08 02:29:01 +0100

    interactive gravity working

--- js/GenericGravitySimulation.js
+++ js/GenericGravitySimulation.js
@@ -1,18 +1,13 @@
 var theCanvas = document.getElementById("theCanvas");
 var theContext = theCanvas.getContext("2d");
+var pauseButton = document.getElementById("pauseButton");
+var resetButton = document.getElementById("resetButton");
 
 var earthRadius = 6371000;
 var mountainHeight = earthRadius * 0.165;
 var newtonG = 6.67e-11;
 var earthMass = 5.97e24;
 var dt = 1;
-
-Particle = {
-    x: Number,
-    y: Number,
-    vx: Number,
-    vy: Number,
-}
 
 listOfParticles = new Array(0);
 
@@ -21,78 +16,114 @@ var metersPerPixel = earthRadius / (0.355 * canvasWidth)
 
 function setupCanvas() {
     theContext.beginPath();
-    theContext.arc(canvasWidth/2, canvasWidth/2, 5, 0, 2*Math.PI);
-    theContext.fillStyle = "red";
+    theContext.arc(canvasWidth/2, canvasWidth/2, 10, 0, 2*Math.PI);
+    theContext.fillStyle = "black";
     theContext.fill();
 }
 
 function propagateParticle(particle) {
     for (let i = 0; i < 10; i++){
-        x = particle.x
-        y = particle.y
-        vx = particle.vx;
-        vy = particle.vy;
+        var x = particle.x;
+        var y = particle.y;
+        var vx = particle.vx;
+        var vy = particle.vy;
 
         var r = Math.sqrt(x*x + y*y);
         var accel = newtonG * earthMass / (r * r);
         var ax = -accel * x / r;
         var ay = -accel * y / r;
-        vx += ax * dt;
-        vy += ay * dt;
-        x += vx * dt;
-        y += vy * dt;
+        particle.vx += ax * dt;
+        particle.vy += ay * dt;
+        particle.x += vx * dt;
+        particle.y += vy * dt;
     }
 }
 
 function drawProjectile(particle) {
-    x = particle.x
-    y = particle.y
-    vx = particle.vx;
-    vy = particle.vy;
+    var x = particle.x
+    var y = particle.y
+    var vx = particle.vx;
+    var vy = particle.vy;
 
     var pixelX = theCanvas.width/2 + x/metersPerPixel;
     var pixelY = theCanvas.height/2 - y/metersPerPixel;
 
-   
-
-    theContext.clearRect(0, 0, theCanvas.width, theCanvas.height)
     theContext.beginPath();
     theContext.arc(pixelX, pixelY, 5, 0, 2*Math.PI);
     theContext.fillStyle = "red";
     theContext.fill();
 }
 
-function addParticle(xPos, yPos) {
-    
-    newParticle = {
-        x: xPos,
-        y: yPos,
-        vx: 6000,
-        vy: 0,
-    }
-    listOfParticles.push(newParticle);
+function addParticle(x, y, vx, vy) {
+    listOfParticles.push(
+        {
+            x: x,
+            y: y,
+            vx: vx,
+            vy: vy,
+        }
+    );
 };
+
+var initialX = 0;
+var initialY = 0;
 
 theCanvas.onmousedown = (e) => {
     var rect = theCanvas.getBoundingClientRect();
-
     var canvasX = e.clientX - rect.x;
     var canvasY = e.clientY - rect.y;
 
-    var x = (canvasX - theCanvas.width/2) * metersPerPixel;
-    var y = - (canvasY - theCanvas.height/2) * metersPerPixel;
+    console.log(canvasX)
 
-    addParticle(x, y)
+    initialX = (canvasX - theCanvas.width/2) * metersPerPixel;
+    initialY = - (canvasY - theCanvas.height/2) * metersPerPixel;
+}
+
+theCanvas.onmouseup = (e) => {
+    var rect = theCanvas.getBoundingClientRect();
+    var canvasX = e.clientX - rect.x;
+    var canvasY = e.clientY - rect.y;
+
+    firedX = (canvasX - theCanvas.width/2) * metersPerPixel;
+    firedY = - (canvasY - theCanvas.height/2) * metersPerPixel;
+
+    var initialVx = (initialX - firedX)/300;
+    var initialVy = (initialY - firedY)/300;
+
+    console.log(initialVx);
+
+    addParticle(firedX, firedY, initialVx, initialVy)
+}
+
+var paused = false;
+
+pauseButton.onmouseup = () => {
+    if (paused) {
+        paused = false;
+        moveProjectiles();
+    } else {
+        paused = true;
+    }
+}
+
+resetButton.onmouseup = () => {
+    theContext.clearRect(0, 0, theCanvas.width, theCanvas.height);
+    setupCanvas();
+    listOfParticles.length = 0;
 }
 
 function moveProjectiles() {
-    for (let p = 0; p < listOfParticles.length; p++) {        
-        propagateParticle(listOfParticles[p]);
-        drawProjectile(listOfParticles[p]);
+    if (!paused) {
+        theContext.clearRect(0, 0, theCanvas.width, theCanvas.height);
+        setupCanvas();
+        for (let p = 0; p < listOfParticles.length; p++) {        
+            propagateParticle(listOfParticles[p]);
+            drawProjectile(listOfParticles[p]);
+        }
+        window.setTimeout(moveProjectiles, 1000/60);
     }
-    window.setTimeout(moveProjectiles, 1000/30);
 }
 
 setupCanvas();
-addParticle(200, 200);
+addParticle(5000, 0);
 moveProjectiles();
